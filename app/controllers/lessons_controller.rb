37d3852fea1cf1b45ea83d4de21ae3d828b47f8c
Ryan Dickinson <ryanjd326@gmail.com>
2019-11-07 19:47:45 +0000

    payments with stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      redirect_to course_path(current_lesson.section.course), alert: 'Not Currently Enrolled'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
